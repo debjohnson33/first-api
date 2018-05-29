99883bad2f860c872e9a543aabe678afc6170ec6
debjohnson <debjohnson_11@yahoo.com>
2018-05-28 19:11:55 -0500

    Added body-parser/edited server to add post

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var ingredients = [
     {
@@ -21,12 +25,22 @@ var ingredients = [
 ];
 
 app.get('/', function(req, res) {
-   res.send('My First API!'); 
+   res.send(ingredients); 
 });
 
 app.get('/funions', function(req, res) {
     res.send('Yo give me some funions foo!');
 })
+
+app.pos('/', function(req, res) {
+   var = ingredient = req.body;
+    if (!ingredient || ingredient.text === "") {
+        res.status(500).send({error: "Your ingredient must have text"});
+    } else {
+        ingredients.push(ingredient);
+        res.status(200).send(ingredient);
+    }
+});
 
 app.listen(3000, function() {
     console.log("First API running on port 3000!");
